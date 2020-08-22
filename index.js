46374dba56ec849406c41f025cf7d043c3530bb1
Gautam Sharma <geekygautam1997@gmail.com>
2020-08-22 23:51:49 +0530

    Update index.js

--- index.js
+++ index.js
@@ -10,6 +10,8 @@ const fileUpload = require("express-fileupload");
 
 const app = express();
 
+const PORT = process.env.PORT || 5000
+
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
 
@@ -80,4 +82,4 @@ app.post("/convert", (req, res) => {
   //.pipe(res, { end: true });
 });
 
-app.listen(5000);
+app.listen(PORT);
